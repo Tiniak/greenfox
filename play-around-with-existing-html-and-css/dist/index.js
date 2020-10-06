c221e76f1e975156959d82031e435d5272edc350
Tiniak <72159552+Tiniak@users.noreply.github.com>
2020-10-06 20:52:12 +0200

    2

--- play-around-with-existing-html-and-css/dist/index.js
+++ play-around-with-existing-html-and-css/dist/index.js
@@ -6,8 +6,15 @@ return function (){return counter +=1;
 }
 })();
 
+function in(){
+    return counter +=1px
+}
+
 function displaycount(){
     document.getElementById("carrier").innerHTML = count();
 }
+document.getElementById("displaycount").addEventListener("click", function() {
+    
+  });
 
  
