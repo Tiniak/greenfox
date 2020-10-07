767e3b16bdaba6b75a636b9d4ba3c12273c091bb
Tiniak <72159552+Tiniak@users.noreply.github.com>
2020-10-07 17:01:20 +0200

    click

--- play-around-with-existing-html-and-css/dist/index.js
+++ play-around-with-existing-html-and-css/dist/index.js
@@ -1,20 +1,36 @@
 
 var count =(function(){
 
-var counter = 0;
-return function (){return counter +=1;
-}
-})();
 
-function in(){
-    return counter +=1px
-}
 
-function displaycount(){
-    document.getElementById("carrier").innerHTML = count();
-}
-document.getElementById("displaycount").addEventListener("click", function() {
+    var counter = 0;
     
-  });
+    return function (){return counter +=1;
+    
+    }
+    
+    })();
+    
+    
+    
+    function in(){
+    
+        return counter +=1px
+    
+    }
+    
+    
+    
+    function displaycount(){
+    
+        document.getElementById("carrier").innerHTML = count();
+    
+    }
+    
+    document.getElementById("displaycount").addEventListener("click", function() {
+    
+        
+    
+      });
 
  
